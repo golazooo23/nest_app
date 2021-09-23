a51e3bd25154159ab18b4ff6e97a858687969104
golazooo23 <kengoteshima0823@gmail.com>
2021-09-23 18:01:33 +0200

    [add]schema.graphql

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { CatsController } from './cats.controller'
 import { CatsService } from './cats.service'
-import { CatsResolver } from './cats.resolver';
+import { CatsResolver } from './cats.resolver'
 
 @Module({
   controllers: [CatsController],
