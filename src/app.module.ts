08d70b1de320664ce3d520b8b45600b63e9d1fd3
golazooo23 <kengoteshima0823@gmail.com>
2021-09-25 11:36:59 +0200

    [add]TypeORMModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common'
 import { CatsModule } from './cats/cats.module'
 import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       playground: true,
       autoSchemaFile: 'schema.graphql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest_app',
+      entities: [],
+      synchronize: false,
     }),
     CatsModule,
   ],
