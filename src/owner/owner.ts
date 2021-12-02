9ff22572d799dbeba0a8548c7b09675542d469b7
golazooo23 <kengoteshima0823@gmail.com>
2021-12-02 10:35:36 +0100

    createRelation

--- src/owner/owner.ts
+++ src/owner/owner.ts
@@ -1,10 +1,10 @@
 import { Field, ID, ObjectType, Int } from '@nestjs/graphql'
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne } from 'typeorm'
-import { Owner } from '../owner/owner'
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, OneToMany } from 'typeorm'
+import { Cat } from '../cats/cat'
 
 @Entity()
 @ObjectType()
-export class Cat {
+export class Owner {
   @PrimaryGeneratedColumn()
   @Field((type) => ID)
   id: number
@@ -21,6 +21,6 @@ export class Cat {
   @Field()
   createdAt: Date
 
-  @ManyToOne(() => Owner, (owner) => owner.cats)
-  owner: Owner
+  @OneToMany(() => Cat, (cat) => cat.owner)
+  cats: Cat[]
 }
